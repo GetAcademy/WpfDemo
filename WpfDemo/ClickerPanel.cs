67262808713f4d0f2d1844824a98a435fbbb34f6
GET Academy <terje@getacademy.no>
2022-08-30 11:24:59 +0200

    x

---
+++ WpfDemo/ClickerPanel.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace WpfDemo
+{
+    internal class ClickerPanel : StackPanel
+    {
+        public ClickerPanel(int clickerCount)
+        {
+            for (var i = 0; i < clickerCount; i++)
+            {
+                Children.Add(new Clicker2());
+            }
+        }
+    }
+}
---
+++ WpfDemo/PushMeButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace WpfDemo
+{
+    internal class PushMeButton : Button
+    {
+        public PushMeButton()
+        {
+            Content = "Trykk på meg!";
+            Background = new SolidColorBrush(Colors.DeepPink);
+        }
+    }
+}
