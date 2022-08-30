f9f1f88d3aeb9e8a47175f67ccea309e7f0f896d
GET Academy <terje@getacademy.no>
2022-08-30 10:59:40 +0200

    Initial commit

---
+++ WpfDemo/Clicker.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfDemo
+{
+    internal class Clicker
+    {
+        Label _label;
+        int _count;
+        public StackPanel Panel { get; }
+
+        public Clicker()
+        {
+            var button = new Button { Content = "Trykk på meg!" };
+            button.Click += ButtonClick;
+            _label = new Label { Content = "0" };
+            Panel = new StackPanel { Orientation = Orientation.Horizontal };
+            Panel.Children.Add(button);
+            Panel.Children.Add(_label);
+        }
+
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            _count++;
+            _label.Content = _count;
+        }
+    }
+}
---
+++ WpfDemo/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfDemo
+{
+    internal class Program
+    {
+        // Pause til 11:00
+
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            var app = new Application();
+            var window = new Window();
+            var clicker1 = new Clicker();
+            var clicker2 = new Clicker();
+            var clicker3 = new Clicker();
+
+            var panel = new StackPanel();
+            panel.Children.Add(clicker1.Panel);
+            panel.Children.Add(clicker2.Panel);
+            panel.Children.Add(clicker3.Panel);
+
+            window.Content = panel;
+            app.Run(window);
+        }
+
+        /*
+         * v1:
+        static Label _label;
+        static int _count;
+
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            var app = new Application();
+            var window = new Window();
+            var button = new Button { Content = "Trykk på meg!" };
+            button.Click += ButtonClick;
+            _label = new Label { Content = "0" };
+            var panel = new StackPanel { Orientation = Orientation.Horizontal };
+            panel.Children.Add(button);
+            panel.Children.Add(_label);
+            window.Content = panel;
+            app.Run(window);
+        }
+
+        private static void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            _count++;
+            _label.Content = _count;
+        }         
+         */
+    }
+}
